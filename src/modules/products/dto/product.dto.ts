c9be315a28b3f82c447e9d4469a7ba379d3801f1
DannielSLl <daniel.supanta.llano@alumnos.uta.cl>
2024-04-22 17:22:47 -0400

    fix/agrega archivos que faltaban en el commit anterior

--- src/modules/products/dto/product.dto.ts
+++ src/modules/products/dto/product.dto.ts
@@ -1,14 +1,26 @@
-export class ProductDto {
+import { IsNotEmpty, IsNumber, Min } from "class-validator";
 
+export class ProductDto {
+    
+    @IsNotEmpty({message: 'el nombre no puede estar vacio'})
     nombre: string;
 
+    @IsNotEmpty({message: 'la descripción no puede estar vacia'})
     description: string;
     
+    @IsNumber()
+    @IsNotEmpty()
+    @Min(1, {message: 'el precio debe de ser al menos de 1'})
     precio: number;
 
+    @IsNumber()
+    @IsNotEmpty({message: 'las calorias no pueden estar vacias'})
     calorias: number;
+
     
+    @IsNotEmpty({message: 'la descripción no puede estar vacia'})
     categoria: string;
 
+    @IsNotEmpty({message: 'la imagen no puede estar vacia'})
     img: string;
 }
