b6eb1dbef1d64aa1c1c9c17c72a39dd1dc68dd27
Bastian Vega <bastianvegagg@gmail.com>
2024-05-03 13:28:22 -0400

    fix: se arreglaron algunas validaciones

--- src/modules/auth/dto/authCredentialsDto.ts
+++ src/modules/auth/dto/authCredentialsDto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsNotEmpty, IsNumber, IsString } from "class-validator";
 export class AuthCredentialsDto {
+
+    @IsNotEmpty()
+    @IsEmail()
     email: string;
+
+    @IsNotEmpty()
     password: string;
   }
   
