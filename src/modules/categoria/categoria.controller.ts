d3bb359d010d0ef475a25c37ab1e3d110cf0059d
vrasantiago <vraniisant@gmail.com>
2024-05-06 17:30:10 -0400

    documentacion categoria

--- src/modules/categoria/categoria.controller.ts
+++ src/modules/categoria/categoria.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { ApiTags, ApiResponse, ApiParam, ApiBody } from '@nestjs/swagger'; // Importar decoradores de Swagger
+import { ApiTags, ApiResponse, ApiParam, ApiBody, ApiOperation } from '@nestjs/swagger'; // Importar decoradores de Swagger
 import { CategoriaService } from './categoria.service';
 import { CategoriaDto } from './dto/categoria.dto';
 
@@ -9,12 +9,14 @@ export class CategoriaController {
   constructor(readonly categoriaSerice: CategoriaService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Obtener todas las categorías.' })
   @ApiResponse({ status: 200, description: 'Devuelve todas las categorías.', type: CategoriaDto, isArray: true })
   async getAll() {
     return await this.categoriaSerice.getAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obtener una categoría por su ID.' })
   @ApiParam({ name: 'id', type: 'number', description: 'ID de la categoría' }) 
   @ApiResponse({ status: 200, description: 'Devuelve una categoría por su ID.', type: CategoriaDto })
   async getOne(@Param('id', ParseIntPipe) id: number) {
@@ -23,6 +25,7 @@ export class CategoriaController {
 
   @UsePipes(new ValidationPipe({ whitelist: true }))
   @Post()
+  @ApiOperation({ summary: 'Crear una nueva categoría.' }) 
   @ApiBody({ type: CategoriaDto }) 
   @ApiResponse({ status: 201, description: 'Crea una nueva categoría.', type: CategoriaDto }) 
   async create(@Body() dto: CategoriaDto) {
@@ -30,6 +33,7 @@ export class CategoriaController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar una categoría por su ID.' }) 
   @ApiParam({ name: 'id', type: 'number', description: 'ID de la categoría' }) 
   @ApiResponse({ status: 200, description: 'Elimina una categoría por su ID.' }) 
   async delete(@Param('id', ParseIntPipe) id: number) {
