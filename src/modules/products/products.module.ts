2ea9531a2b5c29998e3882b73c57207cef362c26
DannielSLl <daniel.supanta.llano@alumnos.uta.cl>
2024-06-23 04:45:34 -0400

    feat: importaciones requeridas

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,20 +1,35 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoriaEntity } from '../categoria/categoria.entity';
 import { ProductEntity } from './product.entity';
 import { ProductsController } from './products.controller';
 import { ProductService } from './products.service';
 import { CategoriaService } from '../categoria/categoria.service';
+import { LocalModule } from '../local/local.module';
+import { LocalProductoEntity } from '../local/local-producto/local-producto.entity';
+import { IngredientesModule } from '../ingredientes/ingredientes.module';
+import { LocalIngredienteEntity } from '../local/local-ingrediente/local-ingrediente.entity';
+import { LocalProductoService } from '../local/local-producto/local-producto.service';
+import { LocalService } from '../local/local/local.service';
 
 @Module({
-    imports: [
+  imports: [
     TypeOrmModule.forFeature([
-        ProductEntity,
-        CategoriaEntity,
-      ]),
-    ],
-    controllers: [ProductsController],
-    providers: [ProductService, CategoriaService],
-    exports: [TypeOrmModule],
+      ProductEntity,
+      LocalProductoEntity,
+      CategoriaEntity,
+    ]),
+    forwardRef(() => LocalModule)
+  ],
+  controllers: [ProductsController],
+  providers: [
+    ProductService,
+    CategoriaService,
+    LocalProductoService,
+    LocalService,
+    ProductService,
+    CategoriaService
+  ],
+  exports: [TypeOrmModule],
 })
 export class ProductsModule {}
